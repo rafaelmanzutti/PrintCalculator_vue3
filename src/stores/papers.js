61127734773ebc108ee4baa827ee4a7c49ea68aa
Rafael Manzutti <manzutti@gmail.com>
2025-02-24 16:00:01 -0400

    ajustes em papers.js; e rename em logicas.js para print.js

--- src/stores/papers.js
+++ src/stores/papers.js
@@ -9,73 +9,102 @@ export const usePaperStore = defineStore('paper', {
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [61, 91, 110, 152],
+        printingPriceMeter: [10, 20, 30, 40],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
       {
         id: 2,
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [61, 91, 110],
+        printingPriceMeter: [10, 20, 30],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
       {
         id: 3,
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [61, 152],
+        printingPriceMeter: [10, 40],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
       {
         id: 4,
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [61, 91],
+        printingPriceMeter: [10, 20],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
       {
         id: 5,
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [61],
+        printingPriceMeter: [10],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
       {
         id: 6,
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [91, 152],
+        printingPriceMeter: [20, 40],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
       {
         id: 7,
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [152],
+        printingPriceMeter: [40],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
       {
         id: 8,
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [61, 91, 110, 152],
+        printingPriceMeter: [10, 20, 30, 40],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
       {
         id: 9,
         name:"",
         type: "",
         margin: "",
-        printingPriceMeter: "",
-        coilWidth: "", //61, 91, 110, 152
+        coilWidth: [110, 152],
+        printingPriceMeter: [30, 40],
+        printingPrices: [],
+        printingLowestPrice: "",
+        coilWidthSelected: ""
       },
+
+
     ],
   }),
 })
--- src/stores/print.js
+++ src/stores/print.js
@@ -4,15 +4,13 @@ import { usePaperStore } from './papers';
 
 const paperStore = usePaperStore();
 
-export const useLogicasStore = defineStore('logicas', {
+export const usePrintStore = defineStore('print', {
   state: () => ({
     papers: [],
     printLongerSide : "",
     printShortSide : "",
-    printLength: "",
-    coilWidthSelected: "",
 
-    totalPrintPrice: "",
+    printLength: "",
 
     frameType: "",
     LinearFramePrice: "",
@@ -25,7 +23,16 @@ export const useLogicasStore = defineStore('logicas', {
   actions: {
     loadPapers(){
       this.papers = paperStore.papers;
-    }
+    },
+    calculatePrices(){
+      for (let i= 0; i<=this.papers.length; i++){
+        for (let j= 0; j<=this.papers[i].coilWidth.length; j++){
+          console.log(this.papers[i].coilWidth[j])
+        }
+      }
+
+
+    },
   }
 })
 
