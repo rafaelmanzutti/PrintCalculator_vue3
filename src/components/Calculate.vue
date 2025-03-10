6baddc334071dc008d36086711456c87d969b4c8
Rafael Manzutti <manzutti@gmail.com>
2025-03-10 11:26:25 -0400

    ajuste de layout em Calculate.vue

--- src/components/Calculate.vue
+++ src/components/Calculate.vue
@@ -20,38 +20,39 @@ const printStore = usePrintStore();
         label="MENOR lado (cm)"
       />
       <template #actions>
-        <v-container
-          fluid
-          class="mt-0 mb-0 pt-0 pb-0"
-        >
-          <v-switch
-            v-model="printStore.frame"
-            label="Colocar moldura"
-            color="orange darken-3"
-            value="orange darken-3"
-            hide-details
-            class="mt-0 mb-0 pt-0 pb-0"
-          />
-        </v-container>
-
         <v-spacer />
-        <v-btn
-          size="small"
-          color="orange-accent-3"
-          variant="elevated"
-          @click="printStore.resetSizes()"
-        >
-          Zerar medidas
-        </v-btn>
-        <v-btn
-          v-show="printStore.printLongerSide > 0 && printStore.printShortSide > 0"
-          color="primary-darken-1"
-          variant="elevated"
-          @click="printStore.checkSides()"
-        >
-          Calcular preço
-        </v-btn>
+        <div>
+          <v-btn
+            size="small"
+            color="orange-accent-3"
+            variant="elevated"
+            @click="printStore.resetSizes()"
+          >
+            Zerar medidas
+          </v-btn>
+          <v-btn
+            v-show="printStore.printLongerSide > 0 && printStore.printShortSide > 0"
+            color="primary-darken-1"
+            variant="elevated"
+            class="ml-2"
+            @click="printStore.checkSides()"
+          >
+            Calcular preço
+          </v-btn>
+        </div>
       </template>
+      <v-container
+        fluid
+        class="mt-0 mb-0 pt-0 pb-0"
+      >
+        <v-switch
+          v-model="printStore.frame"
+          label="Colocar moldura"
+          color="orange darken-3"
+          value="orange darken-3"
+          hide-details="mt-0 mb-0 pt-0 pb-0"
+        />
+      </v-container>
     </v-card>
   </div>
 </template>
